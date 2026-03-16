dc94439db9bbe257c40891ff34598e99584edd30
love-wedding2026 <plyasheva99@gmail.com>
2026-03-16 14:18:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -61,10 +61,16 @@ document.getElementById("seconds").textContent = "00";
     const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
     const seconds = Math.floor((distance % (1000*60)) / 1000);
 
-    document.getElementById("days").textContent = days.toString().padStart(2,'0');
-    document.getElementById("hours").textContent = hours.toString().padStart(2,'0');
-    document.getElementById("minutes").textContent = minutes.toString().padStart(2,'0');
-    document.getElementById("seconds").textContent = seconds.toString().padStart(2,'0');
+    function setNumber(id, value){
+    const el = document.getElementById(id);
+    el.textContent = ""; 
+    el.textContent = value;
+}
+
+setNumber("days", days.toString().padStart(2,'0'));
+setNumber("hours", hours.toString().padStart(2,'0'));
+setNumber("minutes", minutes.toString().padStart(2,'0'));
+setNumber("seconds", seconds.toString().padStart(2,'0'));
 }
 
 updateTimer();
